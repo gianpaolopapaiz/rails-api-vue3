b4a87bad62e7e36033b9f12b30f7472d995dfe70
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-06-04 23:13:21 -0300

    Add axios request to accounts page

--- client/Vue3Front/src/pages/AccountsPage.vue
+++ client/Vue3Front/src/pages/AccountsPage.vue
@@ -1,10 +1,15 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: false,
-  },
-});
+<script>
+  import axios from "axios";
+  export default {
+    mounted() {
+      console.log('Mounted')
+      axios
+          .get('http://localhost:3000/api/v1/accounts')
+          .then(response => (
+              console.log(response)
+          ))
+    }
+  }
 </script>
 
 <template>
@@ -17,25 +22,4 @@ defineProps({
 </template>
 
 <style scoped>
-/*h1 {*/
-/*  font-weight: 500;*/
-/*  font-size: 2.6rem;*/
-/*  top: -10px;*/
-/*}*/
-
-/*h3 {*/
-/*  font-size: 1.2rem;*/
-/*}*/
-
-/*.greetings h1,*/
-/*.greetings h3 {*/
-/*  text-align: center;*/
-/*}*/
-
-/*@media (min-width: 1024px) {*/
-/*  .greetings h1,*/
-/*  .greetings h3 {*/
-/*    text-align: left;*/
-/*  }*/
-/*}*/
 </style>
